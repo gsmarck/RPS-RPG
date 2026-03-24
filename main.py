e8cbcf2ac53f2c9b6b5472df6df82c7d31423273
GS <gsmarck@gmail.com>
2026-03-24 20:19:29 -0300

    Correção de um erro de sintaxe e re-indentação do código
    - Erro na sintaxe (elif sem if) corrigido
    - Indentação usando GH corrigido
    - Edward agora tem apenas 2 tesouras na sua lista de jogadas, uma para cada mão

--- main.py
+++ main.py
@@ -1,49 +1,75 @@
+#GS & M7
 from random import randint
 
+#Listas usadas por cada oponente para decidir a jogada
 oponents = [['pedra', 'papel', 'tesoura'], 
             ['pedra', 'pedra', 'pedra', 'pedra', 'pedra', 'pedra', 'pedra', 'pedra', 'papel', 'tesoura'], 
-            ['pedra', 'papel', 'tesoura', 'tesoura', 'tesoura', 'tesoura'], 
+            ['tesoura', 'tesoura'], 
             ['papel']]
 
 valid_moves = ['pedra', 'papel', 'tesoura']
 
+#Função que escolhe oponete
 def choose():
-    while True:
-        choice = input('Escolha o modo de jogo que deseja jogar digitando o número correspondente e apertando ENTER. \n' \
-                '0 - Aleatorionildo\n' \
-                '1 - Pedro Pedra\n' \
-                '2 - Edward\n' \
-                '3 - Papeleiro Maluco\n')
-        if choice.isnumeric() == True:
-            choice = int(choice)
-            if choice < len(oponents):
-                return int(choice)
+            while True:
+                        choice = input('Escolha o modo de jogo que deseja jogar digitando o número correspondente e apertando ENTER. \n' \
+                                    '0 - Aleatorionildo\n' \
+                                    '1 - Pedro Pedra\n' \
+                                    '2 - Edward\n' \
+                                    '3 - Papeleiro Maluco\n')
+                        
+                        #Filtro de respostas possíveis (Repete até que cumpra ambas as exigências)
+                        if choice.isnumeric() == True:
+                                    choice = int(choice)
+                                    if choice < len(oponents):
+                                                return int(choice)
+                                    print("\nNúmero inválido, escolha dentre as opções apresentadas. \n\n")
+                        print("\nPor favor escreva somente o número do oponente que deseja enfrentar. \n\n")
 
+#Função do jogo em si (usa a resposta da seleção para determinar qual das listas usar)
 def game_start(oponent):
-    pool = oponents[int(oponent)]
-    print('\nEntão vamos começar o jogo. \n\n')
+            #Seleciona a lsita correta para o inimigo
+            pool = oponents[int(oponent)]
+            print('\nEntão vamos começar o jogo. \n\n')
 
-    playerspoints = 0
-    enemyspoints = 0
-    while playerspoints < 2 and enemyspoints < 2:
-        playersplay = str(input('Escolha entre pedra, papel e tesoura para jogar. ')).lower()
-        if playersplay not in valid_moves:
-            continue
-        rand = randint(0, len(pool)-1)
-        enemysplay = pool[rand]
-        elif playersplay == 'tesoura' and enemysplay == 'papel' or playersplay == 'pedra' and enemysplay == 'tesoura' or playersplay == 'papel' and enemysplay == 'pedra':
-            playerspoints += 1
-            print('Você ganhou uma. O placar está: \nVocê: {}\nOponente: {}'.format(playerspoints, enemyspoints))
-        elif playersplay == enemysplay:
-            print('Empate! Ninguém ganha nada. ')
-        else: 
-            enemyspoints += 1
-            print('Você perdeu uma. O placar está: \nVocê: {}\nOponente: {}\n\n'.format(playerspoints, enemyspoints))
+            #Define as variáveis de contagem de pontos
+            playerspoints = 0
+            enemyspoints = 0
 
-    if playerspoints == 2:
-        print('Você ganhou a melhor de três. Parabéns!!!')
-    elif enemyspoints == 2:
-        print('Você perdeu a melhor de três. WOMP WOMP.')
-    print('\nO placar final ficou: \nVocê: {}\nOponente: {}'.format(playerspoints, enemyspoints))
+            #Define que o jogo se repita até que algum dos lados consiga 2 pontos (vitória em melhor de 3)
+            while playerspoints < 2 and enemyspoints < 2:
+                        #Garante que jogada do jogador seja válida (continue prossegue o loop sem executar nenhum comando, 
+                        #Ou seja pode repetir quantas vezes for preciso)
+                        playersplay = str(input('Escolha entre pedra, papel e tesoura para jogar. ')).lower()
+                        if playersplay not in valid_moves:
+                                    print("\nEscolha uma jogada válida (pedra, papel ou tesoura). \n\n")
+                                    continue
+                        #Escolhe aleatoriamente um elemento da lista do oponente
+                        rand = randint(0, len(pool)-1)
+                        enemysplay = pool[rand]
+
+                        #Caso 1: Player ganha
+                        if playersplay == 'tesoura' and enemysplay == 'papel' or \
+                                    playersplay == 'pedra' and enemysplay == 'tesoura' or \
+                                    playersplay == 'papel' and enemysplay == 'pedra':
+                                    playerspoints += 1
+                                    print('Você ganhou uma. O placar está: \nVocê: {}\nOponente: {}'.format(playerspoints, enemyspoints))
+
+                        #Caso 2: Empate
+                        elif playersplay == enemysplay:
+                                    print('Empate! Ninguém ganha nada. ')
+
+                        #Caso 3: Oponente ganha (já que os casos de entrada inválida foram filtrados acima é possível usar o else)
+                        else: 
+                                    enemyspoints += 1
+                                    print('Você perdeu uma. O placar está: \nVocê: {}\nOponente: {}\n\n'.format(playerspoints, enemyspoints))
+
+            if playerspoints == 2:
+                        print('Você ganhou a melhor de três. Parabéns!!!')
+                
+            elif enemyspoints == 2:
+                        print('Você perdeu a melhor de três. WOMP WOMP.')
+                        
+            print('\nO placar final ficou: \nVocê: {}\nOponente: {}'.format(playerspoints, enemyspoints))
 
 game_start(choose())
